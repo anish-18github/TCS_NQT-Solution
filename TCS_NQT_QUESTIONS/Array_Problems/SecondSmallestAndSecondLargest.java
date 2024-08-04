e946f91a8bda421acb189167b94086949e3b3306
anish-18github <108921271+anish-18github@users.noreply.github.com>
2024-08-04 19:48:38 +0530

    Add files via upload

--- TCS_NQT_QUESTIONS/Array_Problems/SecondSmallestAndSecondLargest.java
+++ TCS_NQT_QUESTIONS/Array_Problems/SecondSmallestAndSecondLargest.java
@@ -11,52 +11,52 @@ Explanation:
 
 public class SecondSmallestAndSecondLargest {
     public static void main(String[] args) {
-        int arr[] = {1};
+        int arr[] = { 1, 2, 7, 7, 7, 5 };
 
-        System.out.println("Second Smallest Element in the array: " +secondSmallest(arr));
-
+        System.out.println("Second Smallest Element in the array: " + secondSmallest(arr));
         System.out.println("Second Largest Element in the array: " + secondLargest(arr));
     }
 
-    static int secondSmallest(int arr1[]) {
-
-        int n = arr1.length;
-        if (n < 2) {
-            return -1;
+    static int secondSmallest(int arr[]) {
+         // Check if the array has fewer than 2 elements
+        if (arr.length < 2) {
+            return -1;  // Not enough elements to find the second smallest
         }
 
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = i + 1; j < arr1.length; j++) {
-                int temp = 0;
-                if (arr1[i] > arr1[j]) {
-                    temp = arr1[i];
-                    arr1[i] = arr1[j];
-                    arr1[j] = temp;
-                }
+        int smallest = Integer.MAX_VALUE, secondSmallest = Integer.MAX_VALUE;
+
+         // Iterate over the array to find the smallest and second smallest elements
+        for (int num : arr) {
+            if (num < smallest) {
+                 // Update second smallest before updating smallest
+                secondSmallest = smallest;
+                smallest = num;
+            } else if (num < secondSmallest && num != smallest) {
+                secondSmallest = num;
             }
-            System.out.println(arr1[i]);
         }
-        return arr1[1];
+        // Return -1 if no valid second smallest element is found
+        return (secondSmallest == Integer.MAX_VALUE) ? -1 : secondSmallest;
     }
 
-    static int secondLargest(int arr2[]) {
-
-        int n = arr2.length;
-        if (n < 2) {
+    static int secondLargest(int arr[]) {
+        
+        if (arr.length < 2) {
             return -1;
         }
 
-        for (int i = 0; i < arr2.length; i++) {
-            for (int j = i + 1; j < arr2.length; j++) {
-                int temp = 0;
-                if (arr2[i] > arr2[j]) {
-                    temp = arr2[i];
-                    arr2[i] = arr2[j];
-                    arr2[j] = temp;
-                }
+        int largest = Integer.MIN_VALUE, secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                // Update second largest before updating largest
+                secondLargest = largest;
+                largest = num;
+            }else if (num > secondLargest && num != largest) {
+                secondLargest = num;
             }
         }
-        
-        return arr2[arr2.length - 2];
+
+        return (secondLargest == largest) ? -1 : secondLargest;
     }
 }
