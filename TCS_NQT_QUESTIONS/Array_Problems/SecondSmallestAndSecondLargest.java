e4e538f369830d0fe8093b9df51d025628b0dd09
anish-18github <anishgaikar17@gmail.com>
2024-08-05 14:35:47 +0530

    test

--- TCS_NQT_QUESTIONS/Array_Problems/SecondSmallestAndSecondLargest.java
+++ TCS_NQT_QUESTIONS/Array_Problems/SecondSmallestAndSecondLargest.java
@@ -4,7 +4,7 @@ Input:
  [1,2,4,7,7,5]
 Output:
  Second Smallest : 2
-Second Largest : 5
+Second  : 5
 
 Explanation:
  The elements are as follows 1,2,3,5,7,7 and hence second largest of these is 5 and second smallest is 2 */
