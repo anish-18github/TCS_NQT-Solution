84db6549923d901146dff8302f3aea6aaca8f6ae
anish-18github <anishgaikar17@gmail.com>
2024-08-05 15:50:21 +0530

    reverse array edited

--- TCS_NQT_QUESTIONS/Array_Problems/ReverseArray.java
+++ TCS_NQT_QUESTIONS/Array_Problems/ReverseArray.java
@@ -2,7 +2,7 @@ package TCS_NQT_QUESTIONS.Array_Problems;
 
 public class ReverseArray {
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,7,7,5,3,2,5,7,8};
+        int arr[] = {7,6,5,4,3,2,1};
         int n = arr.length;
         System.out.print("original Array: ");
         for (int i = 0; i < n; i++) {
